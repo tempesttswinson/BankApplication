e2c49df9dfdf0eeaf623487a512c71d076316045
tempesttswinson <tempestt.swinson@gmail.com>
2020-05-24 21:35:15 -0400

    creates accounts based on whether it is stated as checking or savings in the CSV file and then adds each new account to a List of Accounts and shows the info on each by implementing show info method from Account class

--- BankApp.java
+++ BankApp.java
@@ -1,33 +1,38 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankApp {
 
     public static void main(String[] args) {
 
-        String file = "C:\\Users\\user\\WIN\\Notes\\Java\\BankApplication\\utilties\\NewBankAccounts.csv";
-        // Checking chkacc1 = new Checking("Tom Wilson", "123456789", 1500);
-
-        // Savings savacct1 = new Savings("Rich Love", "234567891", 2500);
-
-        // chkacc1.showInfo();
-        // System.out.println("************************************");
-        // savacct1.compound();
-        // savacct1.showInfo();
-
-        // savacct1.deposit(5000);
-        // savacct1.withdraw(200);
-        // savacct1.transfer("Brokerage", 3000);
+        // creates a linked list of objects using the Account class
+        List<Account> accounts = new LinkedList<Account>();
 
         // Read CSV File then create new accounts based on that data
+        String file = "C:\\Users\\user\\WIN\\Notes\\Java\\BankApplication\\utilties\\NewBankAccounts.csv";
         List<String[]> newAccountHolders = utilties.CSV.read(file);
 
         for (String[] accountHolder : newAccountHolders) {
             System.out.println("New Account");
-            System.out.println(accountHolder[0]);
-            System.out.println(accountHolder[1]);
-            System.out.println(accountHolder[2]);
-            System.out.println(accountHolder[3]);
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
+            System.out.println(name + " " + sSN + " " + accountType + "$" + initDeposit);
+            if (accountType.equals("Savings")) {
+                accounts.add(new Savings(name, sSN, initDeposit));
+            } else if (accountType.equals("Checking")) {
+                accounts.add(new Checking(name, sSN, initDeposit));
+            } else {
+                System.out.println("ERROR READING ACCOUNT TYPE");
+            }
+        }
 
+        // iterates through the account list and displays each new account to the
+        // screen`
+        for (Account acc : accounts) {
+            System.out.println("\n***************************");
+            acc.showInfo();
         }
     }
 }
